108a3713291c9e74c13a05a0d925efcaa73ba78b
Kim-Hyunseo <kimhyunseo305@gmail.com>
2022-03-03 09:44:01 +0900

    Main page HTML update

--- html/main/index.js
+++ html/main/index.js
@@ -7,21 +7,6 @@ const LOGIN_SHOW = "loginShowing";
 
 function loginHandle(event) {
     loginForm.classList.toggle(LOGIN_SHOW);
-};
+}
 
 loginBtn.addEventListener("click", loginHandle);
-
-// heart btn edit 
-
-const heartBtn = document.querySelectorAll(".box2_heart")
-
-const HEART_SHOW = "heart_showing";
-
-function heartHandle(event) {
-    const btn = event.target;
-    btn.classList.toggle(HEART_SHOW);
-};
-
-[].forEach.call(heartBtn, function (col) {
-    col.addEventListener("click", heartHandle);
-});
